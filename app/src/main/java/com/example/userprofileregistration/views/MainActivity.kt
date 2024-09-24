6950087fed9638869c4675a3199eb28fcbc5c76d
HandsomeRobot <alaminjoy456@gmail.com>
2024-09-24 13:03:24 +0600

    view done

---
+++ app/src/main/java/com/example/userprofileregistration/views/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.userprofileregistration.views
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.userprofileregistration.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var listButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listButton = findViewById(R.id.showUserListBtn)
+        listButton.setOnClickListener {
+            startActivity(Intent(this, ProfileListActivity::class.java))
+            finish()
+        }
+        }
+    }
